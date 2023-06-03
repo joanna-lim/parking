d2246f784dab388c1cecdea8e10ff5828e880363
Joanna Lim <m210068@e.ntu.edu.sg>
2023-06-04 00:37:05 +0800

    initial commit

---
+++ website/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    
+    with app.app_context():
+        db.create_all()
+
+    return app
+ 
