fecfa74a07476b7ad265398f29da09ed9b852a3b
Joanna Lim <m210068@e.ntu.edu.sg>
2023-06-04 22:45:42 +0800

    added a search box

--- website/static/map.js
+++ website/static/map.js
@@ -8,101 +8,98 @@ var map = new mapboxgl.Map({
 });
 
 async function fetchGeoJSONData() {
-    const response = await fetch("/carparks", {
-      method: "GET"
-    });
-    geojsonData = await response.json();
-    console.log(geojsonData);
-  }
+  const response = await fetch("/carparks", {
+    method: "GET",
+  });
+  geojsonData = await response.json();
+  console.log(geojsonData);
+}
 
-  async function waitTillTargetReady(isTargetReady, milliseconds) {
-    while (!isTargetReady()) {
-      // wait 1 second before retrying
-      await new Promise(resolve => setTimeout(resolve, milliseconds));
-    }
+async function waitTillTargetReady(isTargetReady, milliseconds) {
+  while (!isTargetReady()) {
+    // wait 1 second before retrying
+    await new Promise((resolve) => setTimeout(resolve, milliseconds));
   }
-  
-  function isMapLoaded() {
-    return map && map.loaded();
-  }
-  
+}
+
+function isMapLoaded() {
+  return map && map.loaded();
+}
 
 async function loadGeoJSONData() {
-    try {
-      await fetchGeoJSONData();
-      await waitTillTargetReady(isMapLoaded, 100);
-  
-      map.addSource("carparks-data", {
-        type: "geojson",
-        data: window.geojsonData,
-      });
+  try {
+    await fetchGeoJSONData();
+    await waitTillTargetReady(isMapLoaded, 100);
 
-      const defaultColours = {
-        colour1: "red",
-        colour2: "yellow",
-        colour3: "green",
-      };
-  
-      const shades = defaultColours;
-  
-      // Add a layer containing pins using "carparks-data" source
-      map.addLayer({
-        id: "carparks-layer",
-        type: "circle",
-        source: "carparks-data",
-        paint: {
-          "circle-radius": 12,
-          "circle-color": [
-            "interpolate",
-            ["linear"],
-            ["get", "lots_available"],
-            0,
-            shades.colour1,
-            125,
-            shades.colour2,
-            250,
-            shades.colour3,
-          ],
-          "circle-opacity": 0.6,
-        },
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    map.addSource("carparks-data", {
+      type: "geojson",
+      data: window.geojsonData,
+    });
+
+    const defaultColours = {
+      colour1: "red",
+      colour2: "yellow",
+      colour3: "green",
+    };
+
+    const shades = defaultColours;
+
+    // Add a layer containing pins using "carparks-data" source
+    map.addLayer({
+      id: "carparks-layer",
+      type: "circle",
+      source: "carparks-data",
+      paint: {
+        "circle-radius": 12,
+        "circle-color": [
+          "interpolate",
+          ["linear"],
+          ["get", "lots_available"],
+          0,
+          shades.colour1,
+          125,
+          shades.colour2,
+          250,
+          shades.colour3,
+        ],
+        "circle-opacity": 0.6,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+map.on("zoom", function () {
+  const zoom = map.getZoom();
+
+  map.setPaintProperty("carparks-layer", "circle-opacity", [
+    "interpolate",
+    ["linear"],
+    ["zoom"],
+    10,
+    0,
+    14,
+    0.6,
+    18,
+    1,
+  ]);
+});
+
+loadGeoJSONData();
+
+map.on("click", "carparks-layer", function (e) {
+  var coordinates = e.features[0].geometry.coordinates.slice();
+  var lotsAvailable = e.features[0].properties.lots_available;
+  var address = e.features[0].properties.address;
+
+  var popupContent =
+    "<h3>Lots Available: " + lotsAvailable + "</h3>" +
+    "<p>Address: " + address + "</p>";
+
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
   }
 
-  map.on("zoom", function () {
-    const zoom = map.getZoom();
-  
-    map.setPaintProperty("carparks-layer", "circle-opacity", [
-      "interpolate",
-      ["linear"],
-      ["zoom"],
-      10,
-      0,
-      14,
-      0.6,
-      18,
-      1,
-    ]);
-  });
-
-  loadGeoJSONData();
-
-  map.on("click", "carparks-layer", function (e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var lotsAvailable = e.features[0].properties.lots_available;
-    var address = e.features[0].properties.address;
-  
-    var popupContent = "<h3>Lots Available: " + lotsAvailable + "</h3>" +
-    "<p>Address: " + address + "</p>";
-  
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-  
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(popupContent)
-      .addTo(map);
-  });
+  new mapboxgl.Popup().setLngLat(coordinates).setHTML(popupContent).addTo(map);
+});
