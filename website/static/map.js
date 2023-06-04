0191299c65bb06a6d78de8e6dc097f3cc20a2c1d
Joanna Lim <m210068@e.ntu.edu.sg>
2023-06-04 22:15:23 +0800

    heatmap is up

--- website/static/map.js
+++ website/static/map.js
@@ -1,8 +1,90 @@
 mapboxgl.accessToken = window.MAPBOX_SECRET_KEY;
 
-const map = new mapboxgl.Map({
-container: 'map', 
-style: 'mapbox://styles/mapbox/streets-v12', 
-center: [103.8822, 1.2999], 
-zoom: 15 
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [103.8822, 1.2999],
+  zoom: 15,
 });
+
+async function fetchGeoJSONData() {
+    const response = await fetch("/carparks", {
+      method: "GET"
+    });
+    geojsonData = await response.json();
+    console.log(geojsonData);
+  }
+
+  async function waitTillTargetReady(isTargetReady, milliseconds) {
+    while (!isTargetReady()) {
+      // wait 1 second before retrying
+      await new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+  }
+  
+  function isMapLoaded() {
+    return map && map.loaded();
+  }
+  
+
+async function loadGeoJSONData() {
+    try {
+      await fetchGeoJSONData();
+      await waitTillTargetReady(isMapLoaded, 100);
+  
+      map.addSource("carparks-data", {
+        type: "geojson",
+        data: window.geojsonData,
+      });
+
+      const defaultColours = {
+        colour1: "red",
+        colour2: "yellow",
+        colour3: "green",
+      };
+  
+      const shades = defaultColours;
+  
+      // Add a layer containing pins using "carparks-data" source
+      map.addLayer({
+        id: "carparks-layer",
+        type: "circle",
+        source: "carparks-data",
+        paint: {
+          "circle-radius": 12,
+          "circle-color": [
+            "interpolate",
+            ["linear"],
+            ["get", "lots_available"],
+            0,
+            shades.colour1,
+            125,
+            shades.colour2,
+            250,
+            shades.colour3,
+          ],
+          "circle-opacity": 0.6,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  map.on("zoom", function () {
+    const zoom = map.getZoom();
+  
+    map.setPaintProperty("carparks-layer", "circle-opacity", [
+      "interpolate",
+      ["linear"],
+      ["zoom"],
+      10,
+      0,
+      14,
+      0.6,
+      18,
+      1,
+    ]);
+  });
+
+  loadGeoJSONData();
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 import os
+import json
 
 views = Blueprint('views', __name__)
 
@@ -7,3 +8,9 @@ views = Blueprint('views', __name__)
 def get_map():
    MAPBOX_SECRET_KEY=os.getenv("MAPBOX_SECRET_KEY")
    return render_template("home.html", MAPBOX_SECRET_KEY=MAPBOX_SECRET_KEY)
+
+@views.route("/carparks", methods=["GET"])
+def get_carparks():
+    with open('website/carparks.json') as f:
+        data = json.loads(f.read())
+    return jsonify(data)
