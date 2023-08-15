2b801ee701abb8a934ea18bcb6764e00d6e568dc
Joanna Lim <m210068@e.ntu.edu.sg>
2023-08-15 13:04:27 +0800

    minor error

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET'])
 def get_map():
    MAPBOX_SECRET_KEY=os.getenv("MAPBOX_SECRET_KEY")
-   return render_template("index.html", MAPBOX_SECRET_KEY=MAPBOX_SECRET_KEY)
+   return render_template("home.html", MAPBOX_SECRET_KEY=MAPBOX_SECRET_KEY)
 
 @views.route("/carparks", methods=["GET"])
 def get_carparks():
